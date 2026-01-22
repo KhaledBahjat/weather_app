b0adad7ff32b6faba1c80b357cf9b92ec1d7bf20
Khaled Bahjat <khaledbahjat0@gmail.com>
2026-01-22 10:33:07 -0800

    weather model

--- lib/models/weather_model.dart
+++ lib/models/weather_model.dart
@@ -1,3 +1,31 @@
 class WeatherModel {
-  
+  final String cityName;
+  final String data;
+  final String image;
+  final String avgTemp;
+  final String mxTemp;
+  final String mnTemp;
+  final String weatherCondition;
+
+  WeatherModel({
+    required this.cityName,
+    required this.data,
+    required this.image,
+    required this.avgTemp,
+    required this.mxTemp,
+    required this.mnTemp,
+    required this.weatherCondition,
+  });
+  factory WeatherModel.fromJson(json) {
+    return WeatherModel(
+      cityName: json["location"]["name"],
+      data: json["current"]["last_updated"],
+      avgTemp: json["forecast"]["forecastday"][0]["day"]["avgtemp_c"],
+      mxTemp: json["forecast"]["forecastday"][0]["day"]["maxtemp_c"],
+      mnTemp: json["forecast"]["forecastday"][0]["day"]["mintemp_c"],
+      weatherCondition:
+          json["forecast"]["forecastday"][0]["day"]["condition"]["text"],
+      image: json["forecast"]["forecastday"][0]["day"]["condition"]["icon"],
+    );
+  }
 }
