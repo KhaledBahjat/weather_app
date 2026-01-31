2c0db65dd25ece16b878d575d0ab69bf709b4201
Khaled Bahjat <khaledbahjat0@gmail.com>
2026-01-31 00:57:41 -0800

    refactor

--- lib/cubits/get_weather_cubit/get_weather_cubit.dart
+++ lib/cubits/get_weather_cubit/get_weather_cubit.dart
@@ -1,20 +1,31 @@
+import 'dart:developer';
+
 import 'package:bloc/bloc.dart';
 import 'package:dio/dio.dart';
 import 'package:weather_app/cubits/get_weather_cubit/get_weather_state.dart';
 import 'package:weather_app/models/weather_model.dart';
 import 'package:weather_app/services/weather_service.dart';
 
-class GetWeatherCubit extends Cubit<WeatherState> {
-  GetWeatherCubit() : super(NoWeatherState());
-
+class GetWeatherCubit extends Cubit<WeatherStates> {
+  GetWeatherCubit() : super(InitialWeatherState());
+  WeatherModel? weatherData;
   getCurrentWeather({required String cityName}) async {
+    emit(LoadingState());
+    await Future.delayed(Duration(seconds: 1));
     try {
-      WeatherModel weatherModel = await WeatherService(
+      weatherData = await WeatherService(
         Dio(),
       ).getCurrentWeather(cityName: cityName);
-      emit(WeatherLoadingState());
-    } on Exception {
-      emit(WeatherFailureState());
+      emit(GetWeatherState());
+    } on Exception catch (e) {
+      log(e.toString());
+      if (e.toString() == 'Exception: No matching location found.') {
+        emit(ErrorCityNameState());
+      } else if (e.toString() == 'Exception: No NetWork') {
+        emit(ErrorNetWorkState());
+      } else if (e.toString() == 'Exception: Parameter q is missing.') {
+        emit(NoEnterCityState());
+      }
     }
   }
 }
